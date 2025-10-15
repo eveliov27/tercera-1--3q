c5e47c48de29e461903b310f70ed27e376c5333e
eveliov27 <evelioj91@gmail.com>
2025-10-15 17:42:33 -0600

    Creacion de Proyecto

---
+++ vistas/vista_superior.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="Spanish">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="Tercera 1" content="Proyecto Universitario">
+    <title>Tercera 1</title>
+    <link rel="stylesheet" href="./css/main.css">
+    
+    
+
+
+</head>
